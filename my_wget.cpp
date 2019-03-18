b89748fdb698724a46e438e6e9829e60da773ac4
vitaly.razgulyaev <vitaly.razgulyaev@dressformer.com>
2019-03-18 20:27:41 +0300

    Parse urls

--- my_wget.cpp
+++ my_wget.cpp
@@ -1,15 +1,16 @@
 #include <fstream>
 #include <iostream>
 #include <string_view>
+#include <sstream>
 
 #include "network.h"
 enum class Status { OK = 200 };
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "Url command line parameter is required\n";
-    return 1;
-  }
+ // if (argc != 2) {
+    std::cerr << "Url commandline parameter is required\n";
+   // return 1;
+  //}
 
   // https://cdn2.hubspot.net/hub/416323/file-2337009921-png/HomeImages/V2/logo_uipath_blue.png?t=1453382687840
   // https://browser.yandex.ru/download?from=wizard__yabro_one_&banerid=0500000000&os=win
@@ -17,7 +18,22 @@ int main(int argc, char *argv[]) {
       "https://dl.bintray.com/boostorg/release/1.69.0/source/"
       "boost_1_69_0.tar.gz"; //*/
 
-  argv[1] = "cdn2.hubspot.net";
+    argv[1] = "https://cdn2.hubspot.net/hub/416323/file-2337009921-png/HomeImages/V2/logo_uipath_blue.png?t=1453382687840";
+  
+  std::string url(argv[1]);
+  std::transform(url.begin(), url.end(), url.begin(), ::tolower);
+  std::string_view url_view(url);
+  size_t pos = 0;
+  if((pos=url_view.find("://")) != -1) {
+      url_view.remove_prefix(pos + 3);
+  }
+  
+  pos = url_view.find('/');
+  std::string host(url_view.substr(0, pos));
+  std::string_view path =url_view.substr(pos, -1);
+  pos = url_view.rfind('/');
+  size_t param_pos = url_view.find('?', pos);
+  std::string  filename(url_view.substr(pos + 1, param_pos-pos -1));
   struct addrinfo hints;
   struct addrinfo *result, *rp;
 
@@ -32,7 +48,7 @@ int main(int argc, char *argv[]) {
   hints.ai_next = nullptr;
 
   std::cout << "Resolver host address\n";
-  auto status = getaddrinfo(argv[1], "80", /*nullptr*/ &hints, &result);
+  auto status = getaddrinfo(host.c_str(), "80",  &hints, &result);
   if (status != 0) {
     std::cerr << "Error: " << hstrerror(h_errno) << std::endl;
     return 1;
@@ -62,56 +78,49 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  std::string request =
+    std::stringstream ss;
+    ss <<
       "GET "
-      "/hub/416323/file-2337009921-png/HomeImages/V2/"
-      "logo_uipath_blue.png?t=1453382687840 HTTP/1.1\r\n"
-      "Host: cdn2.hubspot.net\r\n"
+       << path << " HTTP/1.1\r\n"
+      "Host: " << host << "\r\n"
       "User-Agent: my_wget\r\n"
       "Accept: */*\r\n"
       "Connection: close\r\n"
       "\r\n";
-
-  write(sock_fd, request.data(), request.size() * sizeof(char));
+    auto request = ss.str();
+  write(sock_fd, request.c_str(), request.size() * sizeof(char));
 
   std::cout << "Starting download from: " << argv[1] << std::endl;
   ssize_t n;
-  int content = 0;
 
   // read header
-  char recv_line[MAXLINE + 1];
-  int line_num = 0;
-  while ((n = read(sock_fd, recv_line, MAXLINE)) > 0) {
-    recv_line[n] = 0;
-    std::string_view view(recv_line);
-    size_t pos = 0;
-    size_t end_line = 0;
-
-    while (end_line != std::string_view::npos) {
-      end_line = view.find_first_of("\r\n", pos);
-      if (end_line != std::string_view::npos) {
-        auto line = view.substr(pos, end_line - pos);
-        std::cout << line;
-        ++line_num;
-        if (line_num == 1 && line.find("200") == std::string_view::npos)
-          return 1;
-
-        auto del_pos = line.find(":");
-        if (content == 0 &&
-            line.find("Content-Length:") != std::string_view::npos)
-          content = std::stoi(std::string(line.substr(del_pos + 1)));
-
-        pos = end_line + 2;
-      }
-    }
+  std::string header;
+  header.reserve(MAXLINE);
+    
+  char ch;
+  while ((n = read(sock_fd, &ch, 1)) > 0) {
+    header.push_back(ch);
+      if (header.size() >= 4 && std::string_view(header.c_str() + header.size() - 4) =="\r\n\r\n")
+          break;
   }
-
-  // read content;
-  std::ofstream out("test.png", std::ios::binary);
-  recv_line[n] = 0;
-  out.write(recv_line, n);
-
-  std::cout << std::endl;
+    
+  if (n < 0) {
+    std::cerr << "Read error" << std::strerror(errno) << std::endl;
+    return 1;
+  }
+    
+    if (header.find("200 OK") == std::string::npos){ // TODO redirect
+        std::cerr << "The server doesnot return HTTP OK response\n";
+        return 1;
+    }
+    
+  // read content
+  char recv_line[MAXLINE + 1];
+  std::ofstream out(filename, std::ios::binary);
+  while ((n = read(sock_fd, recv_line, MAXLINE)) > 0) {
+      recv_line[n] = '\0';
+      out.write(recv_line, n);
+  }
   if (n < 0) {
     std::cerr << "Read error" << std::strerror(errno) << std::endl;
   }
